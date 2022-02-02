a561abdf7b03e941aa3902c3a61930ae6f569de1
Ashish Ranjan <codewithaashu1@gmail.com>
2022-02-02 15:41:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Code With Aashu by me
         </a>
       </header>
     </div>
